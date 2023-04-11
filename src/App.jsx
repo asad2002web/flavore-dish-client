233600d14882cdcda7524d533a1e0bbd271724cc
asad2002web <asad2002web@gmail.com>
2023-04-12 01:14:39 +0600

    error page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h2>Amiu all ok</h2>
+    <div className="App mx-10">
+      <h3>ok</h3>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import ErrorPage from './component/ErrorPage'
+
+const route = createBrowserRouter([
+  {
+    path:'/',
+    element: <App></App>,
+    errorElement: <ErrorPage></ErrorPage>
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider 
+    router={route}
+    ></RouterProvider>
 )
